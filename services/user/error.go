0c89651f8cb90f68e6d5b535466b7e46312c8ff6
eric <11235171+eolso@users.noreply.github.com>
2024-05-04 11:23:42 -0700

    Add more errors for the user service

--- services/user/error.go
+++ services/user/error.go
@@ -3,6 +3,8 @@ package user
 import "errors"
 
 var (
-	ErrNotFound = errors.New("no user found")
-	ErrConflict = errors.New("user already exists")
+	ErrNotFound        = errors.New("no user found")
+	ErrConflict        = errors.New("user already exists")
+	ErrInvalidUsername = errors.New("username is invalid")
+	ErrInvalidPassword = errors.New("password must be at least 8 characters")
 )
